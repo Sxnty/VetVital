15b2f3119ef2f6ed25a028c08c34fd48909fbb77
DevAG <programador.andresgarcia@gmail.com>
2023-04-13 22:38:45 -0700

    Fixed Ui problems

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,25 @@ import Team from "./components/pages/Team";
 import Operation from "./components/pages/Operation";
 import Inventory from "./components/pages/Inventory";
 import Billing from "./components/pages/Billing";
+import "./styles/global-styles/index.scss";
 
 import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/schedule" element={<Schedule />}></Route>
-        <Route path="/patients" element={<Patients />}></Route>
-        <Route path="/team" element={<Team />}></Route>
-        <Route path="/operation" element={<Operation />}></Route>
-        <Route path="/inventory" element={<Inventory />}></Route>
-        <Route path="/billing" element={<Billing />}></Route>
-      </Routes>
+      <main className="main">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/schedule" element={<Schedule />}></Route>
+          <Route path="/patients" element={<Patients />}></Route>
+          <Route path="/team" element={<Team />}></Route>
+          <Route path="/operation" element={<Operation />}></Route>
+          <Route path="/inventory" element={<Inventory />}></Route>
+          <Route path="/billing" element={<Billing />}></Route>
+        </Routes>
+      </main>
     </>
   );
 }
