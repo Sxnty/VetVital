02dd3212e19b04bce216ea4d49225e94e9715912
Sxnty <sxntycsgo@gmail.com>
2023-04-17 20:47:55 -0300

    Added addPatient and deletePatient to Firestore.

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -5,6 +5,8 @@ import {
   where,
   doc,
   getDoc,
+  addDoc,
+  deleteDoc,
 } from "firebase/firestore/lite";
 import { db } from "./firebase";
 
@@ -17,9 +19,9 @@ export const getPatients = async () => {
         id: doc.id,
         ...doc.data(),
       };
-      /*       console.log(document); */
       return document;
     });
+    console.log(result);
     return { code: 200, msg: result };
   } else {
     return { code: 500, msg: [] };
@@ -38,4 +40,21 @@ export const getOnePatient = async (id) => {
   } else {
     return { code: 500, msg: `No se encontró el paciente con ID ${id}` };
   }
+};
+
+export const addPatient = async (patient) => {
+  if (patient) {
+    let result = await addDoc(collection(db, "pacientes"), patient);
+    if (result && result.type === "document") {
+      return { code: 200, id: result.id };
+    } else {
+      return { code: 500, msg: "Error al añadir paciente" };
+    }
+  }
+};
+
+export const deletePatient = async (id) => {
+  let patientRef = collection(db, "pacientes");
+  await deleteDoc(doc(patientRef, id));
+  return { code: 200, msg: "Paciente eliminada correctamente" };
 };
